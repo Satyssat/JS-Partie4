3c26b53cfb1cc364bb6f95783d98f690b1d34585
satyssat <couyssat.satya@gmail.com>
2021-09-03 19:31:50 +0200

    User-App

--- cours/index.js
+++ cours/index.js
@@ -8,6 +8,58 @@
 //     console.log(err);
 //   });
 
-fetch("test.txt")
-  .then((res) => res.text())
-  .then((data) => (document.body.innerHTML = `<h2>${data}</h2>`));
+// fetch("test.txt")
+//   .then((res) => res.text())
+//   .then((data) => (document.body.innerHTML += `<h2>${data}</h2>`));
+
+const myHeaders = new Headers();
+
+const init = {
+  method: "GET",
+  headers: myHeaders,
+  mode: "cors",
+  cache: "default",
+};
+
+//CRUD => C = Create (POST), R = Read(GET), U = Update(PUT), D = Delete(DELETE)
+
+const init2 = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    pseudo: "Satya",
+    message: "Yo les gens :)",
+  }),
+  mode: "cors",
+  credentials: "same-origin",
+};
+
+document.querySelector("form").addEventListener("submit", () => {
+  fetch("http://localhost:3000/posts", init2).then(() =>
+    console.log("test positif!")
+  );
+});
+
+//-------------------
+// ASYNCHRONE
+//----------------
+
+setTimeout(() => {
+  console.log(`j'apparais 2 secondes après`);
+}, 2000);
+
+//-----------------
+// Promise
+//-----------------
+
+fetch("monLien").then((res) => res);
+
+//async/await
+
+async function fetchData() {
+  await fetch("monLien"); // j'attend d'avoir fetché "monLien" avant de passer à la suite du code
+
+  console.log("c'est bon !");
+}
---
+++ user-app/index.js
@@ -0,0 +1,12 @@
+let userData = [];
+
+const fetchUser = () => {
+  fetch("https://randomuser.me/api/?results=24")
+    .then((res) => res.json())
+    .then((data) => (userData = data.results));
+  setTimeout(() => {
+    console.log(userData);
+  }, 1000);
+};
+
+fetchUser();
