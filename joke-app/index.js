2eacde9ed985f5e0a382764feefcf8648116a051
satyssat <couyssat.satya@gmail.com>
2021-09-03 17:14:42 +0200

    Joke-App

--- joke-app/index.js
+++ joke-app/index.js
@@ -1,10 +1,19 @@
 const header = document.getElementById("header");
 const content = document.getElementById("content");
+const app = document.querySelector(".app");
 
-console.log(content, header);
-fetch("https://api.blablagues.net/?rub=blagues")
-  .then((res) => res.json())
-  .then((data) => {
-    header.textContent = data.data.content.text_head;
-    content.textContent = data.data.content.text;
-  });
+function getJoke() {
+  fetch("https://api.blablagues.net/?rub=blagues")
+    .then((res) => res.json())
+    .then((data) => {
+      const joke = data.data.content;
+      header.textContent = joke.text_head;
+      content.textContent = joke.text !== "" ? joke.text : joke.text_hidden;
+    });
+}
+
+// app.addEventListener("click", getJoke());
+getJoke();
+app.addEventListener("click", () => {
+  getJoke();
+});
